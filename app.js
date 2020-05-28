ba6415c02c613b17c4cf753d08edeca53d401092
Anirudh gangwar <anirudhgangwar444@gmail.com>
2020-05-28 14:17:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 var express=require('express');
 
 var app=express();
-var port =process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var service_ip=process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
-app.get('/',function(req, res){
+app.get('/',(req, res){
     res.send("Hello this is a demo project for Minishift");
 });
-app.listen(port,service_ip,function(){
-    console.log("Listening on PORT "+ port);
-})
+var server = app.listen(8080, function(){
+    var port = server.address().port;
+    console.log("Server started at http://localhost:%s", port);
+});
